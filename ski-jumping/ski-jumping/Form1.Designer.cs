ef1e7c21ad64c6583159ea1c992b48405c22fa08
kimo77 <77kimo@gmail.com>
2017-02-27 09:02:01 +0200

    New ski jumping project

---
+++ ski-jumping/ski-jumping/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ski_jumping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KPointNud = new System.Windows.Forms.NumericUpDown();
+            this.KPointLbl = new System.Windows.Forms.Label();
+            this.JumpLengthNud = new System.Windows.Forms.NumericUpDown();
+            this.JumpLengthLbl = new System.Windows.Forms.Label();
+            this.CalcBtn = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.KPointNud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JumpLengthNud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // KPointNud
+            // 
+            this.KPointNud.Location = new System.Drawing.Point(86, 5);
+            this.KPointNud.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.KPointNud.Name = "KPointNud";
+            this.KPointNud.Size = new System.Drawing.Size(59, 20);
+            this.KPointNud.TabIndex = 0;
+            // 
+            // KPointLbl
+            // 
+            this.KPointLbl.AutoSize = true;
+            this.KPointLbl.Location = new System.Drawing.Point(13, 12);
+            this.KPointLbl.Name = "KPointLbl";
+            this.KPointLbl.Size = new System.Drawing.Size(40, 13);
+            this.KPointLbl.TabIndex = 1;
+            this.KPointLbl.Text = "K-point";
+            // 
+            // JumpLengthNud
+            // 
+            this.JumpLengthNud.Location = new System.Drawing.Point(86, 31);
+            this.JumpLengthNud.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.JumpLengthNud.Name = "JumpLengthNud";
+            this.JumpLengthNud.Size = new System.Drawing.Size(59, 20);
+            this.JumpLengthNud.TabIndex = 0;
+            // 
+            // JumpLengthLbl
+            // 
+            this.JumpLengthLbl.AutoSize = true;
+            this.JumpLengthLbl.Location = new System.Drawing.Point(13, 38);
+            this.JumpLengthLbl.Name = "JumpLengthLbl";
+            this.JumpLengthLbl.Size = new System.Drawing.Size(64, 13);
+            this.JumpLengthLbl.TabIndex = 1;
+            this.JumpLengthLbl.Text = "Jump length";
+            // 
+            // CalcBtn
+            // 
+            this.CalcBtn.Location = new System.Drawing.Point(16, 227);
+            this.CalcBtn.Name = "CalcBtn";
+            this.CalcBtn.Size = new System.Drawing.Size(129, 23);
+            this.CalcBtn.TabIndex = 2;
+            this.CalcBtn.Text = "Calculate";
+            this.CalcBtn.UseVisualStyleBackColor = true;
+            this.CalcBtn.Click += new System.EventHandler(this.CalcBtn_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultTextBox.Location = new System.Drawing.Point(16, 69);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(129, 35);
+            this.resultTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.CalcBtn);
+            this.Controls.Add(this.JumpLengthLbl);
+            this.Controls.Add(this.KPointLbl);
+            this.Controls.Add(this.JumpLengthNud);
+            this.Controls.Add(this.KPointNud);
+            this.Name = "Form1";
+            this.Text = "Ski jumping";
+            ((System.ComponentModel.ISupportInitialize)(this.KPointNud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JumpLengthNud)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown KPointNud;
+        private System.Windows.Forms.Label KPointLbl;
+        private System.Windows.Forms.NumericUpDown JumpLengthNud;
+        private System.Windows.Forms.Label JumpLengthLbl;
+        private System.Windows.Forms.Button CalcBtn;
+        private System.Windows.Forms.TextBox resultTextBox;
+    }
+}
+
---
+++ ski-jumping/ski-jumping/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ski_jumping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcBtn_Click(object sender, EventArgs e)
+        {
+            int kPoint = Convert.ToInt32(KPointNud.Value);
+            int jumpLen = Convert.ToInt32(JumpLengthNud.Value);
+            float pointsPerMeter = 0;
+
+            float resultFloat;
+
+            if (kPoint < 20) pointsPerMeter = 0;
+            else if (kPoint >= 20 && kPoint <= 24) pointsPerMeter = 4.8f;
+            else if (kPoint >= 25 && kPoint <= 29) pointsPerMeter = 4.4f;
+            else if (kPoint >= 30 && kPoint <= 34) pointsPerMeter = 4f;
+            else if (kPoint >= 35 && kPoint <= 39) pointsPerMeter = 3.6f;
+            else if (kPoint >= 40 && kPoint <= 49) pointsPerMeter = 3.2f;
+            else if (kPoint >= 50 && kPoint <= 59) pointsPerMeter = 2.8f;
+            else if (kPoint >= 60 && kPoint <= 69) pointsPerMeter = 2.4f;
+            else if (kPoint >= 70 && kPoint <= 79) pointsPerMeter = 2.2f;
+            else if (kPoint >= 80 && kPoint <= 99) pointsPerMeter = 2f;
+            else if (kPoint >= 100 && kPoint <= 169) pointsPerMeter = 1.8f;
+            else if (kPoint >= 170) pointsPerMeter = 1.2f;
+
+            resultFloat = (jumpLen - kPoint) * pointsPerMeter + 60;
+
+            resultTextBox.Text = resultFloat.ToString();
+        }
+    }
+}
